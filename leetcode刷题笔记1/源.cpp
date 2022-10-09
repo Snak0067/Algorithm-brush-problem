ffcb03fa1d1216930d8430d5f6c547ace1e226e0
xiaofeng <1244762145@qq.com>
2022-10-09 22:19:44 +0800

    每日十题  day2

--- leetcode刷题笔记1/源.cpp
+++ leetcode刷题笔记1/源.cpp
@@ -5,8 +5,17 @@
 #include<string>
 #include<cmath>
 #include<algorithm>
+#include<map>
+#include<stack>
+#include<queue>
 using namespace std;
-
+const int INF = 999999999;
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
 struct ListNode {
 	int val;
 	ListNode* next;
@@ -75,7 +84,7 @@ int search(vector<int>& nums, int target) {
 	return -1;
 }
 //153. 寻找旋转排序数组中的最小值
-int findMin(vector<int>& nums) {
+int findMin1(vector<int>& nums) {
 	int len = nums.size(), i = 0, j = len - 1;
 	if (nums[i] < nums[len - 1])return nums[i];
 	while (i < j) {
@@ -261,17 +270,467 @@ int minSubArrayLen(int target, vector<int>& nums) {
 	return cnt == 999 ? 0 : cnt;
 }
 //149. 直线上最多的点数
+bool pointSort(vector<int>v1, vector<int>v2) {
+	if (v1[0] != v2[0])return v1[0] < v2[0];
+	return v1[1] < v2[1];
+}
 int maxPoints(vector<vector<int>>& points) {
+	vector<vector<int>>v;
+	unordered_map<string, int>k;
+	unordered_map<string, vector<int>>same;
+	int len = points.size();
+	for (int i = 0; i < points.size(); i++)
+	{
+		string s = to_string(points[i][1]) + "_" + to_string(points[i][0]);
+		if (same.find(s) != same.end())continue;
+		same[s] = points[i];
+		v.push_back(points[i]);
+	}
+	for (int i = 0; i < v.size(); i++)
+	{
+		for (int j = i + 1; j < v.size(); j++)
+		{
+			double rate = (v[j][1] * 1.0 - v[i][1]) / (v[j][0] * 1.0 - v[i][0]);
+			double b = v[j][1] - rate * v[j][0];
+			string id = to_string(rate) + "_" + to_string(b);
+			k[id]++;
+		}
+	}
+	int max = -1;
+	auto iter = k.begin();
+	while (iter != k.end()) {
+		if (iter->second > max)max = iter->second;
+		iter++;
+	}
+	int cnt = 1;
+	while (cnt < len && (cnt - 1) * cnt != max * 2) {
+		cnt++;
+	}
+	return cnt;
+}
+//2427. 公因子的数目
+int commonFactors(int a, int b) {
+	int count = 0;
+	for (int i = 0; i <= a; i++)
+	{
+		if (a % i == 0 && b % i == 0) {
+			count++;
+		}
+	}
+	return count;
+}
+//2428. 沙漏的最大总和
+int findMaxSumOfSand(vector<vector<int>>& grid, int width, int height, int i, int j) {
+	int numa = i, numb = height + i - 1, tempw = width, max = 0, temp = j;
+	while (numa != numb) {
+		for (int i = temp; i < tempw + temp; i++)
+		{
+			max += grid[numa][i];
+			max += grid[numb][i];
+		}
+		numa++;
+		numb--;
+		tempw -= 2;
+		temp++;
+	}
+	max += grid[numa][temp];
+	return max;
+}
+int maxSumOfSand(vector<vector<int>>& grid) {
+	int max = 0;
+	for (int i = 0; i <= grid.size() - 3; i++) {
+		for (int j = 0; j <= grid[i].size() - 3; j++)
+		{
+			int temp = findMaxSumOfSand(grid, 3, 3, i, j);
+			if (max < temp) {
+				max = temp;
+			}
+		}
+	}
+	return max;
+}
+//2429. 最小 XOR
+//int minimizeXor(int num1, int num2) {
+//	int c1 = __builtin_popcount(num1);
+//	int c2 = __builtin_popcount(num2);
+//	for (; c2 < c1; ++c2) num1 &= num1 - 1; // 最低的 1 变成 0
+//	for (; c2 > c1; --c2) num1 |= num1 + 1; // 最低的 0 变成 1
+//	return num1;
+//}
+//剑指 Offer II 001. 整数除法
+//int divide(int a, int b) {
+//
+//}
+int findRepeatNumber(vector<int>& nums) {
+	map<int, int>mmap;
+	for (int num : nums) {
+		if (mmap.find(num) != mmap.end())return num;
+		mmap[num]++;
+	}
+}
+//剑指 Offer 04. 二维数组中的查找
+bool findNumberIn2DArray1(vector<vector<int>>& matrix, int target) {
+	int height = matrix.size(), width = matrix[0].size();
+	int ha = 0, hb = height - 1, wa = 0, wb = width - 1;
+	while (ha < height && matrix[ha][width - 1] < target)ha++;
+	while (hb >= 0 && matrix[hb][0] > target)hb--;
+	while (wa < width && matrix[height - 1][wa] < target)wa++;
+	while (wb >= 0 && matrix[0][wb] > target)wb--;
+	if (wb < 0 || hb < 0 || wa >= width || ha >= height)return false;
+	for (int i = ha; i <= hb; i++)
+	{
+		for (int j = wa; j <= wb; j++)
+		{
+			if (matrix[i][j] == target)return true;
+		}
+	}
+	return false;
+}
+//类似查找二叉搜索树
+bool findNumberIn2DArray(vector<vector<int>>& matrix, int target) {
+	if (matrix.size() == 0 || matrix[0].size() == 0)return false;
+	int i = matrix[0].size() - 1, j = 0;
+	while (i >= 0 && j < matrix.size()) {
+		if (matrix[j][i] < target)j++;
+		else if (matrix[j][i] > target)i--;
+		else return true;
+	}
+	return false;
+}
+//剑指 Offer 05. 替换空格
+string replaceSpace(string s) {
+	string ans;
+	for (char ch : s) {
+		if (ch == ' ')ans += "%20";
+		else ans.push_back(ch);
+	}
+	return ans;
+}
+//剑指 Offer 06. 从尾到头打印链表
+vector<int> reversePrint(ListNode* head) {
+	vector<int>ans;
+	while (head != nullptr) {
+		ans.push_back(head->val);
+		head = head->next;
+	}
+	reverse(ans.begin(), ans.end());
+	return ans;
+}
+//剑指 Offer 07. 前序后序求树
+TreeNode* buildTreeHelp(vector<int>& pre, vector<int>& in, int ini, int inj, int prei, int prej) {
+	TreeNode* head = new TreeNode(pre[prei]);
+	int root = pre[prei], index = ini;
+	while (index <= inj && in[index] != root)index++;
+	int left = index - ini, right = inj - index;
+	if (left > 0)head->left = buildTreeHelp(pre, in, ini, index - 1, prei + 1, prei + left);
+	if (right > 0)head->right = buildTreeHelp(pre, in, index + 1, inj, prei + 1 + left, prej);
+	return head;
+}
+TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+	if (preorder.size() == 0 || inorder.size() == 0)return nullptr;
+	return buildTreeHelp(preorder, inorder, 0, inorder.size() - 1, 0, preorder.size() - 1);
+}
+//剑指 Offer 09. 用两个栈实现队列
+class CQueue {
+public:
+	stack<int>a, b;
+	CQueue() {
 
+	}
+
+	void appendTail(int value) {
+		a.push(value);
+	}
+
+	int deleteHead() {
+		if (b.empty()) {
+			if (a.empty()) {
+				return -1;
+			}
+			while (!a.empty()) {
+				b.push(a.top());
+				a.pop();
+			}
+		}
+		int ans = b.top();
+		b.pop();
+		return ans;
+	}
+};
+//剑指 Offer 10- I. 斐波那契数列
+int fib(int n) {
+	vector<long long>fibtemp;
+	fibtemp.push_back(0);
+	fibtemp.push_back(1);
+	int cnt = 2;
+	while (cnt <= n) {
+		fibtemp.push_back((fibtemp[cnt - 2] + fibtemp[cnt - 1]) % 1000000007);
+		cnt++;
+	}
+	return fibtemp[n];
+}
+//剑指 Offer 10- II. 青蛙跳台阶问题
+int numWays(int n) {
+	vector<int>way;
+	way.push_back(1);
+	way.push_back(1);
+	int cnt = 2;
+	while (n >= cnt) {
+		way.push_back((way[cnt - 1] + way[cnt - 2]) % 1000000007);
+		cnt++;
+	}
+	return way[n];
+}
+//剑指 Offer 11. 旋转数组的最小数字
+int minArray(vector<int>& numbers) {
+	int i = 0, j = numbers.size() - 1;
+	while (i < j) {
+		int mid = (i + j) / 2;
+		if (numbers[mid] < numbers[j])j = mid;
+		else if (numbers[mid] > numbers[j])i = mid + 1;
+		else j--;
+	}
+	return numbers[i];
+}
+//154. 寻找旋转排序数组中的最小值 II
+int findMin2(vector<int>& nums) {
+	int i = 0, j = nums.size() - 1;
+	while (i < j) {
+		int mid = (i + j) / 2;
+		if (nums[mid] < nums[j])j = mid;
+		else if (nums[mid] > nums[j])i = mid + 1;
+		else j--;
+	}
+	return nums[i];
+}
+//18. 四数之和
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+	int len = nums.size();
+	vector<vector<int>>ans;
+	if (nums.size() < 4)return ans;
+	map<string, vector<int>>mmap;
+	sort(nums.begin(), nums.end());
+	long  targetNum = (long)target;
+	for (int i = 0; i < nums.size() - 3; i++)
+	{
+		for (int j = i + 1; j < nums.size() - 2; j++) {
+			int newt = targetNum - nums[i] - nums[j];
+			int left = j + 1, right = nums.size() - 1;
+			if ((nums[len - 1] + nums[len - 2]) < newt) continue;
+			if ((nums[j + 1] + nums[j + 2]) > newt) continue;
+			while (left < right) {
+				if ((long)nums[left] + (long)nums[right] < newt)left++;
+				else if ((long)nums[left] + (long)nums[right] > newt)right--;
+				else {
+					string tab = to_string(nums[i]) + "_" + to_string(nums[j])
+						+ "_" + to_string(nums[left]) + "_" + to_string(nums[right]);
+					if (mmap.find(tab) == mmap.end()) {
+						mmap[tab] = { nums[i],nums[j] ,nums[left] ,nums[right] };
+						ans.push_back({ nums[i],nums[j] ,nums[left] ,nums[right] });
+					}
+					left++;
+					right--;
+				}
+			}
+		}
+	}
+	return ans;
+}
+//20. 有效的括号
+bool isValid(string s) {
+	int n = s.size();
+	if (n % 2 == 1) {
+		return false;
+	}
+	map<char, char>mmap = { {')','('},{']','['},{'}','{'} };
+	stack<char>sta;
+	for (char ch : s) {
+		if (mmap.find(ch) != mmap.end()) {
+			if (sta.empty() || sta.top() != mmap[ch]) {
+				return false;
+			}
+			sta.pop();
+		}
+		else sta.push(ch);
+	}
+	return sta.empty();
+}
+//21. 合并两个有序链表  
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+	if (list1 == nullptr || list2 == nullptr)return list1 == nullptr ? list2 : list1;
+	ListNode head, * tail = &head, * aptr = list1, * bptr = list2;
+	while (aptr && bptr) {
+		if (aptr->val < bptr->val) {
+			tail->next = aptr;
+			aptr = aptr->next;
+		}
+		else {
+			tail->next = bptr;
+			bptr = bptr->next;
+		}
+		tail = tail->next;
+	}
+	tail->next = (aptr == nullptr ? bptr : aptr);
+	return head.next;
+}
+//23. 合并K个升序链表 分而治之 分治排序
+ListNode* merge(vector<ListNode*>& lists, int l, int r) {
+	if (l == r)return lists[l];
+	if (l > r)return nullptr;
+	int mid = (l + r) >> 1;
+	return mergeTwoLists(merge(lists, l, mid), merge(lists, mid + 1, r));
+}
+ListNode* mergeKLists(vector<ListNode*>& lists) {
+	return merge(lists, 0, lists.size() - 1);
+}
+//24. 两两交换链表中的节点
+ListNode* swapPairs(ListNode* head) {
+	if (head == nullptr || head->next == nullptr)return head;
+	ListNode* newhead = head->next;
+	head->next = swapPairs(newhead->next);
+	newhead->next = head;
+	return newhead;
+}
+//25. K 个一组翻转链表
+ListNode* reverseKGroup(ListNode* head, int k) {
+	vector<int>v;
+	stack<int> s;
+	int cnt = 0;
+	while (head) {
+		if (cnt != 0 && cnt % k == 0) {
+			while (!s.empty()) {
+				v.push_back(s.top());
+				s.pop();
+			}
+		}
+		s.push(head->val);
+		head = head->next;
+		cnt++;
+	}
+	if (!s.empty()) {
+		if (s.size() % k == 0) {
+			while (!s.empty()) {
+				v.push_back(s.top());
+				s.pop();
+			}
+		}
+		else {
+			stack<int>news;
+			while (!s.empty()) {
+				news.push(s.top());
+				s.pop();
+			}
+			while (!news.empty()) {
+				v.push_back(news.top());
+				news.pop();
+			}
+		}
+
+	}
+	ListNode root, * tail = &root;
+	for (int val : v) {
+		tail->next = new ListNode(val);
+		tail = tail->next;
+	}
+	return root.next;
+}
+//26. 删除有序数组中的重复项
+int removeDuplicates(vector<int>& nums) {
+	int n = nums.size();
+	if (n == 0) {
+		return 0;
+	}
+	int l = 1, r = 1;
+	while (r < nums.size()) {
+		if (nums[r] != nums[r - 1]) {
+			nums[l] = nums[r];
+			l++;
+		}
+		r++;
+	}
+	return l;
+}
+//27. 移除元素
+int removeElement(vector<int>& nums, int val) {
+	int l = 0, r = 0;
+	while (r < nums.size()) {
+		if (nums[r] != val) {
+			swap(nums[l], nums[r]);
+			l++;
+		}
+		r++;
+	}
+	return l;
+}
+//28. 找出字符串中第一个匹配项的下标
+int strStr(string haystack, string needle) {
+	return haystack.find(needle);
+}
+//29. 两数相除
+int divide(int dividend, int divisor) {
+	if (dividend == INT_MIN) {
+		if (divisor == 1)return INT_MIN;
+		if (divisor == -1)return INT_MAX;
+	}
+	if (divisor == INT_MIN) {
+		return (dividend == INT_MIN ? 1 : 0);
+	}
+	if (dividend == 0)return 0;
+	int sigh = -1, result = 0;
+	if (dividend > 0 && divisor > 0 || dividend < 0 && divisor < 0)sigh = 1;
+	long dividend_long = (long)abs(dividend);
+	long divisor_long = (long)abs(divisor);
+	while (dividend_long >= divisor_long) {
+		long i = 1;
+		long temp = divisor_long;
+		while (dividend_long >= temp) {
+			dividend_long -= temp;
+			result += i;
+			i = i << 1;
+			temp = temp << 1;
+		}
+	}
+	result *= sigh;
+	if (result > INT_MAX || result < INT_MIN) {
+		return INT_MAX;
+	}
+	return (int)result;
+}
+//30. 串联所有单词的子串
+//vector<int> findSubstring(string s, vector<string>& words) {
+//
+//}
+//856. 括号的分数
+int scoreOfParentheses(string s) {
+	if (s.size() == 2)return 1;
+	int cnt = 0, len = 0;
+	for (int i = 0; i < s.size(); i++)
+	{
+		cnt += (s[i] == ')' ? -1 : 1);
+		if (cnt == 0) {
+			len = i + 1;
+			break;
+		}
+	}
+	if (len == s.size()) {
+		return 2 * scoreOfParentheses(s.substr(1, len - 2));
+	}
+	else return scoreOfParentheses(s.substr(0, len)) + scoreOfParentheses(s.substr(len));
 }
 
 
 
 int main() {
 	ListNode* root = nullptr, * temp = nullptr;
-	vector<int>nums = { 10,5,2,6,7,8,4,1 };
-	vector<vector<int>>a = { {3,5},{9,20} };
-	vector<vector<int>>b = { {4, 5}, { 7,10 }, { 11,12 }, { 14,15 }, { 16,20 } };
+	vector<int>nums = { 0,1,2,2,3,0,4,2 };
+	vector<vector<int>>a = {
+  {1,   4,  7, 11, 15},
+  {2,   5,  8, 12, 19},
+  {3,   6,  9, 16, 22},
+  {10, 13, 14, 17, 24},
+  {18, 21, 23, 26, 30}
+	};
+	vector<int>preorder = { 3, 9, 20, 15, 7 }, inorder = { 9, 3, 15, 20, 7 };
 	for (int x : nums) {
 		if (root == nullptr) {
 			root = new ListNode(x);
@@ -282,7 +741,8 @@ int main() {
 			temp = temp->next;
 		}
 	}
-	numSubarrayProductLessThanK(nums, 100);
+
+	scoreOfParentheses("(()(()))");
 
 	return 0;
 }
