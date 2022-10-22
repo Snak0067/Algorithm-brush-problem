c6bc5c1f3c26ad230f9cf2b04531a48fd77bc572
xiaofeng <1244762145@qq.com>
2022-10-22 11:14:56 +0800

    87.扰乱字符串 动态规划

--- leetcode刷题笔记1/源.cpp
+++ leetcode刷题笔记1/源.cpp
@@ -2322,13 +2322,10 @@ ListNode* reverseBetween(ListNode* head, int left, int right) {
 	return root->next;
 }
 //104. 二叉树的最大深度
-class maxDepthSolution {
-public:
-	int maxDepth(TreeNode* root) {
-		if (root == nullptr)return 0;
-		return 1 + max(maxDepth(root->left), maxDepth(root->right));
-	}
-};
+int maxDepth(TreeNode* root) {
+	if (root == nullptr)return 0;
+	return 1 + max(maxDepth(root->left), maxDepth(root->right));
+}
 //111. 二叉树的最小深度
 int minDepth(TreeNode* root) {
 	if (root == nullptr)return 0;
@@ -2343,7 +2340,99 @@ int minDepth(TreeNode* root) {
 	}
 	return 1;
 }
+//87. 扰乱字符串
+class ScrambleSolution {
+private:
+	// 记忆化搜索存储状态的数组
+	// -1 表示 false，1 表示 true，0 表示未计算
+	int memo[30][30][31];
+	string s1, s2;
 
+public:
+	bool checkIfSimilar(int i1, int i2, int length) {
+		unordered_map<int, int> freq;
+		for (int i = i1; i < i1 + length; ++i) {
+			++freq[s1[i]];
+		}
+		for (int i = i2; i < i2 + length; ++i) {
+			--freq[s2[i]];
+		}
+		if (any_of(freq.begin(), freq.end(), [](const auto& entry) {return entry.second != 0; })) {
+			return false;
+		}
+		return true;
+	}
+
+	// 第一个字符串从 i1 开始，第二个字符串从 i2 开始，子串的长度为 length，是否和谐
+	bool dfs(int i1, int i2, int length) {
+		if (memo[i1][i2][length]) {
+			return memo[i1][i2][length] == 1;
+		}
+
+		// 判断两个子串是否相等
+		if (s1.substr(i1, length) == s2.substr(i2, length)) {
+			memo[i1][i2][length] = 1;
+			return true;
+		}
+
+		// 判断是否存在字符 c 在两个子串中出现的次数不同
+		if (!checkIfSimilar(i1, i2, length)) {
+			memo[i1][i2][length] = -1;
+			return false;
+		}
+
+		// 枚举分割位置
+		for (int i = 1; i < length; ++i) {
+			// 不交换的情况
+			if (dfs(i1, i2, i) && dfs(i1 + i, i2 + i, length - i)) {
+				memo[i1][i2][length] = 1;
+				return true;
+			}
+			// 交换的情况
+			if (dfs(i1, i2 + length - i, i) && dfs(i1 + i, i2, length - i)) {
+				memo[i1][i2][length] = 1;
+				return true;
+			}
+		}
+
+		memo[i1][i2][length] = -1;
+		return false;
+	}
+
+	bool isScramble(string s1, string s2) {
+		memset(memo, 0, sizeof(memo));
+		this->s1 = s1;
+		this->s2 = s2;
+		return dfs(0, 0, s1.size());
+	}
+};
+class isScrambleSolution {
+public:
+	bool isScramble(string s1, string s2) {
+		if (s1.size() != s2.size()) return false;
+		if (s1 == s2) return true;
+		int len = s1.size();
+		int m[256] = { 0 };
+		for (int i = 0; i < len; i++) {
+			m[s1[i]]++;
+			m[s2[i]]--;
+		}
+		for (int i = 0; i < 256; i++) {
+			if (m[i] != 0)return false;
+		}
+		for (int i = 1; i < len; i++) {
+			if (isScramble(s1.substr(0, i), s2.substr(0, i))
+				&& isScramble(s1.substr(i, len - i), s2.substr(i, len - i))) {
+				return true;
+			}
+			if (isScramble(s1.substr(0, i), s2.substr(len - i, i))
+				&& isScramble(s1.substr(i, len - i), s2.substr(0, len - i))) {
+				return true;
+			}
+		}
+		return false;
+	}
+};
 
 int main() {
 	ListNode* root = nullptr, * temp = nullptr;
@@ -2357,6 +2446,7 @@ int main() {
 		{'0','1','1','1','1','1','1','0','1','0'},{'0','0','1','1','1','1','1','1','1','0'},{'1','1','0','1','0','1','1','1','1','0'},
 		{'0','0','0','1','1','0','0','0','1','0'},{'1','1','0','1','1','0','0','1','1','1'},{'0','1','0','1','1','0','1','0','1','1'} };
 	vector<string>s = { "This", "is", "an", "example", "of", "text", "justification." };
-	reverseBetween(root, 1, 2);
+	isScrambleSolution is;
+	is.isScramble("great", "rgeat");
 	return 0;
 }
