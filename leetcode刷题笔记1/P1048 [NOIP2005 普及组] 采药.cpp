ca15aa39a748a32e98237f705adc2c41b4e10d83
xiaofeng <1244762145@qq.com>
2022-11-12 15:35:09 +0800

    01背包问题 背包容量作为j

--- leetcode刷题笔记1/P1048 [NOIP2005 普及组] 采药.cpp
+++ leetcode刷题笔记1/P1048 [NOIP2005 普及组] 采药.cpp
@@ -12,7 +12,28 @@
 #include<limits.h>
 using namespace std;
 
+int dp[105][1005];
 int main() {
+	int time, number;
+	cin >> time >> number;
+	vector<int>v(number + 1), t(number + 1);
+	for (int i = 1; i <= number; i++)
+	{
+		cin >> t[i] >> v[i];
+	}
+	for (int i = 1; i <= number; i++)
+	{
+		for (int j = time; j >= 0; j--)
+		{
+			if (j >= t[i]) {
+				dp[i][j] = max(dp[i - 1][j - t[i]] + v[i], dp[i - 1][j]);
+			}
+			else {
+				dp[i][j] = dp[i - 1][j];
+			}
+		}
+	}
+	cout << dp[number][time] << endl;
 
 	return 0;
 }
