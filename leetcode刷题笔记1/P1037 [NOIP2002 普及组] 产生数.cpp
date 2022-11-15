e7399cfa3c2ee815baa77e2073c50997aeeeeee4
xiaofeng <1244762145@qq.com>
2022-11-15 19:39:29 +0800

    产生数 明日再战

--- leetcode刷题笔记1/P1037 [NOIP2002 普及组] 产生数.cpp
+++ leetcode刷题笔记1/P1037 [NOIP2002 普及组] 产生数.cpp
@@ -1,3 +1,53 @@
+////#include<iostream>
+////#include<unordered_map>
+////#include<set>
+////#include<vector>
+////#include<string>
+////#include<cmath>
+////#include<algorithm>
+////#include<map>
+////#include<stack>
+////#include<unordered_set>
+////#include<queue>
+////#include<limits.h>
+////using namespace std;
+////unordered_map<int, unordered_set<int>>rule;
+////int main() {
+////	string s;
+////	int n;
+////	cin >> s >> n;
+////	for (int i = 0; i < n; i++)
+////	{
+////		int origin, changed;
+////		cin >> origin >> changed;
+////		rule[origin].insert(changed);
+////	}
+////	bool changed = true;
+////	while (changed) {
+////		changed = false;
+////		for (auto it = rule.begin(); it != rule.end(); it++) {
+////			unordered_set<int>changeSet = it->second;
+////			for (auto i = changeSet.begin(); i != changeSet.end(); i++)
+////			{
+////				for (auto j = rule[*i].begin(); j != rule[*i].end(); j++)
+////				{
+////					if (it->first == *j)continue;
+////					if (!rule[it->first].count(*j)) {
+////						rule[it->first].insert(*j);
+////						changed = true;
+////					}
+////				}
+////			}
+////		}
+////	}
+////	int cnt = 1;
+////	for (int i = 0; i < s.length(); i++)
+////	{
+////		cnt *= (1 + rule[s[i] - '0'].size());
+////	}
+////	cout << cnt << endl;
+////	return 0;
+////}
 //#include<iostream>
 //#include<unordered_map>
 //#include<set>
@@ -11,40 +61,45 @@
 //#include<queue>
 //#include<limits.h>
 //using namespace std;
-//unordered_map<int, unordered_set<int>>rule;
+//int s[33], num[33], suffix[33][16];
+//int vis[10];
+//int cnt = 1;
+//void dfs(int x, int y) {
+//	if (vis[y])return;
+//	vis[x] = 1;
+//	cnt++;
+//	for (int i = 0; i < num[y]; i++)
+//	{
+//		dfs(y, suffix[y][i]);
+//	}
+//	vis[x] = 0;
+//}
 //int main() {
-//	string s;
-//	int n;
-//	cin >> s >> n;
+//	int n, k;
+//	string str;
+//	cin >> str >> k;
+//	n = str.length();
 //	for (int i = 0; i < n; i++)
 //	{
-//		int origin, changed;
-//		cin >> origin >> changed;
-//		rule[origin].insert(changed);
+//		s[i] = str[i] - '0';
 //	}
-//	bool changed = true;
-//	while (changed) {
-//		changed = false;
-//		for (auto it = rule.begin(); it != rule.end(); it++) {
-//			unordered_set<int>changeSet = it->second;
-//			for (auto i = changeSet.begin(); i != changeSet.end(); i++)
-//			{
-//				for (auto j = rule[*i].begin(); j != rule[*i].end(); j++)
-//				{
-//					if (it->first == *j)continue;
-//					if (!rule[it->first].count(*j)) {
-//						rule[it->first].insert(*j);
-//						changed = true;
-//					}
-//				}
-//			}
-//		}
-//	}
-//	int cnt = 1;
-//	for (int i = 0; i < s.length(); i++)
+//	for (int i = 0; i < k; i++)
 //	{
-//		cnt *= (1 + rule[s[i] - '0'].size());
+//		int x, y;
+//		cin >> x >> y;
+//		suffix[x][num[x]] = y;
+//		num[x]++;
 //	}
-//	cout << cnt << endl;
+//	int res = 1;
+//	for (int i = 0; i < n; i++)
+//	{
+//		cnt = 1;
+//		for (int j = 0; j < num[s[i]]; j++)
+//		{
+//			dfs(s[i], suffix[s[i]][j]);
+//		}
+//		res *= cnt;
+//	}
+//	cout << res << endl;
 //	return 0;
 //}
