a77d8aa71ff78b24c004c7935bbcded97b6c07a5
xiaofeng <1244762145@qq.com>
2022-11-15 16:15:23 +0800

    数的统计

---
+++ leetcode刷题笔记1/P1097 [NOIP2007 提高组] 统计数字.cpp
@@ -0,0 +1,40 @@
+//#include<iostream>
+//#include<unordered_map>
+//#include<set>
+//#include<vector>
+//#include<string>
+//#include<cmath>
+//#include<algorithm>
+//#include<map>
+//#include<stack>
+//#include<unordered_set>
+//#include<queue>
+//#include<limits.h>
+//using namespace std;
+//vector<long long>v;
+//int main() {
+//	int n;
+//	cin >> n;
+//	v.resize(n);
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> v[i];
+//	}
+//	sort(v.begin(), v.end());
+//	long long cur = v[0];
+//	int time = 1;
+//	for (int i = 1; i < n; i++)
+//	{
+//		if (cur == v[i]) {
+//			time++;
+//			continue;
+//		}
+//		else {
+//			printf("%lld %d\n", cur, time);
+//			time = 1;
+//			cur = v[i];
+//		}
+//	}
+//	printf("%lld %d\n", cur, time);
+//	return 0;
+//}
