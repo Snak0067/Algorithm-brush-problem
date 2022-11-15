9b914da7172236242437560756cb0c2748e8f59c
xiaofeng <1244762145@qq.com>
2022-11-15 19:04:33 +0800

    40分代码

--- leetcode刷题笔记1/P1037 [NOIP2002 普及组] 产生数.cpp
+++ leetcode刷题笔记1/P1037 [NOIP2002 普及组] 产生数.cpp
@@ -11,15 +11,40 @@
 //#include<queue>
 //#include<limits.h>
 //using namespace std;
-//vector<pair<int, int>>rule;
+//unordered_map<int, unordered_set<int>>rule;
 //int main() {
 //	string s;
 //	int n;
 //	cin >> s >> n;
-//	rule.resize(n);
 //	for (int i = 0; i < n; i++)
 //	{
-//
+//		int origin, changed;
+//		cin >> origin >> changed;
+//		rule[origin].insert(changed);
 //	}
+//	bool changed = true;
+//	while (changed) {
+//		changed = false;
+//		for (auto it = rule.begin(); it != rule.end(); it++) {
+//			unordered_set<int>changeSet = it->second;
+//			for (auto i = changeSet.begin(); i != changeSet.end(); i++)
+//			{
+//				for (auto j = rule[*i].begin(); j != rule[*i].end(); j++)
+//				{
+//					if (it->first == *j)continue;
+//					if (!rule[it->first].count(*j)) {
+//						rule[it->first].insert(*j);
+//						changed = true;
+//					}
+//				}
+//			}
+//		}
+//	}
+//	int cnt = 1;
+//	for (int i = 0; i < s.length(); i++)
+//	{
+//		cnt *= (1 + rule[s[i] - '0'].size());
+//	}
+//	cout << cnt << endl;
 //	return 0;
 //}
