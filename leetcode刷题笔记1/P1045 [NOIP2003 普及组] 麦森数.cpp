cfe5144ce14d32225d27ee3d24e2198486c9b114
xiaofeng <1244762145@qq.com>
2022-11-15 19:56:00 +0800

    麦素数

---
+++ leetcode刷题笔记1/P1045 [NOIP2003 普及组] 麦森数.cpp
@@ -0,0 +1,36 @@
+//#include<iostream>
+//#include<unordered_map>
+//#include<set>
+//#include<vector>
+//#include<string>
+//#include<cmath>
+//#include<algorithm>
+//#include<map>
+//#include<stack>
+//#include<unordered_set>
+//#include<queue>
+//#include<limits.h>
+//using namespace std;
+//int ans[505];
+//unordered_map<int, vector<int>>memory;
+//vector<int>mul(vector<int>a, vector<int>b) {
+//	vector<int>ans;
+//	int alen = a.size(), blen = b.size();
+//	for (int i = 0; i < alen; i++)
+//	{
+//		int flag = 0, cur = 0;
+//		for (int j = 0; j < blen; j++)
+//		{
+//			int temp = a[i] * b[j] + flag;
+//
+//		}
+//
+//	}
+//}
+//int main() {
+//	int p;
+//	cin >> p;
+//	long long first = pow(2, 40);
+//	cout << first << endl;
+//	return 0;
+//}
