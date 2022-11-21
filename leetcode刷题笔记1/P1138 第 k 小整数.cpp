47cc44e9201848581ec35bf2e01c3b61cfbfed79
xiaofeng <1244762145@qq.com>
2022-11-21 18:19:08 +0800

    第k小整数

--- leetcode刷题笔记1/P1138 第 k 小整数.cpp
+++ leetcode刷题笔记1/P1138 第 k 小整数.cpp
@@ -1,18 +1,37 @@
-#include<iostream>
-#include<unordered_map>
-#include<set>
-#include<vector>
-#include<string>
-#include<cmath>
-#include<algorithm>
-#include<map>
-#include<stack>
-#include<unordered_set>
-#include<queue>
-#include<limits.h>
-using namespace std;
-
-int main() {
-
-	return 0;
-} 
+//#include<iostream>
+//#include<unordered_map>
+//#include<set>
+//#include<vector>
+//#include<string>
+//#include<cmath>
+//#include<algorithm>
+//#include<map>
+//#include<stack>
+//#include<unordered_set>
+//#include<queue>
+//#include<limits.h>
+//using namespace std;
+//vector<int>v;
+//int main() {
+//	int n, rank;
+//	cin >> n >> rank;
+//	v.resize(n);
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> v[i];
+//	}
+//	sort(v.begin(), v.end());
+//	int cur = v[0], cnt = rank - 1;
+//	for (int i = 0; i < n; i++)
+//	{
+//		if (v[i] == cur)continue;
+//		cur = v[i];
+//		cnt--;
+//		if (cnt == 0) {
+//			cout << cur << endl;
+//			return 0;
+//		}
+//	}	
+//	printf("NO RESULT");
+//	return 0;
+//}
