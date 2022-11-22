e78a374fa0794331ba07d22642ec67da049de049
xiaofeng <1244762145@qq.com>
2022-11-22 15:28:43 +0800

    p1090 优先队列

---
+++ leetcode刷题笔记1/P1090 [NOIP2004 提高组] 合并果子.cpp
@@ -0,0 +1,33 @@
+//#include<iostream>
+//#include<unordered_map>
+//#include<set>
+//#include<vector>
+//#include<string>
+//#include<cmath>
+//#include<algorithm>
+//#include<map>
+//#include<stack>
+//#include<queue>
+//#include<limits.h>
+//using namespace std;
+//priority_queue<int, vector<int>, greater<int>>q;
+//int main() {
+//	int n, num;
+//	cin >> n;
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> num;
+//		q.push(num);
+//	}
+//	long cost = 0;
+//	while (q.size() > 1) {
+//		int first = q.top();
+//		q.pop();
+//		int second = q.top();
+//		q.pop();
+//		cost += (first + second);
+//		q.push(first + second);
+//	}
+//	cout << cost << endl;
+//	return 0;
+//}
