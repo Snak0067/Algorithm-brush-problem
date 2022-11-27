ac5a15b1eabc5479ede759b99c502c9bb94098fb
xiaofeng <1244762145@qq.com>
2022-11-27 16:34:13 +0800

    p1164动态规划

--- leetcode刷题笔记1/P1164 小A点菜.cpp
+++ leetcode刷题笔记1/P1164 小A点菜.cpp
@@ -1,32 +1,64 @@
-#include<iostream>
-#include<unordered_map>
-#include<set>
-#include<vector>
-#include<string>
-#include<cmath> 
-#include<algorithm>
-#include<map>
-#include<stack>
-#include<unordered_set>
-#include<queue>
-#include<limits.h>
-using namespace std;
-int v[105];
-int main() {
-	int n, m;
-	cin >> n >> m;
-	for (int i = 0; i < n; i++)
-	{
-		cin >> v[i];
-	}
-	sort(v, v + n);
-	int left = 0, right = 0;
-	m -= v[0];
-	int cnt = 0;
-	while (right < n && left < n) {
-		if (m > 0) {
-
-		}
-	}
-	return 0;
-}
+//#include<iostream>
+//#include<unordered_map>
+//#include<set>
+//#include<vector>
+//#include<string>
+//#include<cmath> 
+//#include<algorithm>
+//#include<map>
+//#include<stack>
+//#include<unordered_set>
+//#include<queue>
+//#include<limits.h>
+//using namespace std;
+//int v[105];
+//int sum[105];
+//int temp;
+//int n, m, ans = 0;
+///*搜索方法，只能过一些样例简单的测试点*/
+//void dfs(int i) {
+//	int lastSum = temp + sum[n] - sum[i - 1];
+//	if (temp > m || i > n + 1 || lastSum < m) {
+//		return;
+//	}
+//	if (temp == m) {
+//		ans++;
+//		return;
+//	}
+//	temp += v[i];
+//	dfs(i + 1);
+//	temp -= v[i];
+//	dfs(i + 1);
+//}
+//void searchTest() {
+//	dfs(1);
+//	cout << ans << endl;
+//}
+///*dp动态规划解法*/
+////前i道菜花了j元的办法的总数
+//int dp[105][10005] = { 0 };
+//int main() {
+//	cin >> n >> m;
+//	for (int i = 1; i <= n; i++)
+//	{
+//		cin >> v[i];
+//		sum[i] = sum[i - 1] + v[i];
+//	}
+//	for (int i = 1; i <= n; i++)
+//	{
+//		for (int j = 1; j <= m; j++) {
+//			if (j == v[i]) {
+//				dp[i][j] = dp[i - 1][j] + 1;
+//			}
+//			if (j > v[j]) {
+//				dp[i][j] = dp[i - 1][j] + dp[i - 1][j - v[i]];
+//			}
+//			if (j < v[i]) {
+//				dp[i][j] = dp[i - 1][j];
+//			}
+//		}
+//
+//	}
+//	cout << dp[n][m] << endl;
+//	return 0;
+//}
